94196efbc2d590f579f7dc81662a77e1f4f3e729
YannPoncet <yann.poncetpro@gmail.com>
2019-04-03 14:37:04 +0200

    Adding our name to /student

--- src/main/groovy/projet/gl51/StudentController.groovy
+++ src/main/groovy/projet/gl51/StudentController.groovy
@@ -2,14 +2,17 @@ package projet.gl51
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpStatus
-
+// import io.micronaut.http.HttpStatus
 
 @Controller("/student")
 class StudentController {
 
     @Get("/")
-    HttpStatus index() {
-        return HttpStatus.OK
+    List<Student> index() {
+        [
+                new Student(firstName: "Boris", lastName: "Brogle"),
+                new Student(firstName: "Yann", lastName: "Poncet"),
+                new Student(firstName: "Rodolphe", lastName:"Ponthon")
+        ]
     }
 }
